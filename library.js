4fe631b3b95932c5f9e744a7742481fe0cbc359d
xlrn <ncl.yim@gmail.com>
2021-09-18 14:51:57 -0700

    added delete functionality

--- library.js
+++ library.js
@@ -18,10 +18,14 @@ function renderBook(book) {
     let div = document.createElement('div');
     div.setAttribute('id', book.title);
     div.setAttribute('class', 'book');
+    if (book.read == true) {
+        div.style.backgroundColor = "aquamarine";
+    } else div.style.backgroundColor = "lightcoral";
     div.textContent = `${book.title}, by ${book.author}`;
     let readButton = createButton("Read", book.title);
     let deleteButton = createButton("Delete", book.title);
     readButton.addEventListener('click', changeColour);
+    deleteButton.addEventListener('click', deleteBook);
     div.appendChild(readButton);
     div.appendChild(deleteButton);
     container.appendChild(div);
@@ -37,11 +41,19 @@ function createButton(className, title) {
     return button;
 }
 
+function deleteBook() {
+    let title = this.parentNode.id;
+    let book = myLibrary.find(element => element.title == title);
+    let index = myLibrary.indexOf(book);
+    myLibrary.splice(index, 0);
+    this.parentNode.remove();
+}
+
 function changeColour() {
     let parent = this.parentNode.style;
-    if (parent.backgroundColor == "green") {
-        parent.backgroundColor = "red"
-    } else parent.backgroundColor = "green";
+    if (parent.backgroundColor == "aquamarine") {
+        parent.backgroundColor = "lightcoral"
+    } else parent.backgroundColor = "aquamarine";
 }
 
 function renderLibrary(library) {
@@ -78,9 +90,9 @@ function handleSubmit() {
     const read = document.querySelector('#readInput');
     let readBool = false;
     let pageValue = parseInt(pages.value);
-    if (read.value == "on") {
+    if (read.checked) {
         readBool = true;
-    }
+    } else readBool = false;
     let newBook = new Book(title.value, author.value, pageValue, readBool);
     addBookToLibrary(newBook, myLibrary);
     renderBook(newBook);
